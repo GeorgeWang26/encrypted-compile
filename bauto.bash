adbc496327af4651cc63e011cad0df92869bb42d
ZhengWang26 <wzheng2013@gmail.com>
2022-06-02 11:04:11 -0400

    update bauto.bsh

--- bauto.bash
+++ bauto.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 colcon build
 ws=$(pwd)
-echo $ws
+echo -e "\n\n----------------------------\n$ws"
 python_version="3.$(cut -d "." -f 2 <<< $(python3 --version))"
 echo "python version: $python_version"
 
@@ -9,7 +9,7 @@ rm -r backup
 mkdir backup
 
 for d in $ws/src/*/ ; do
-    echo -e "\n\n---------------------\n$d"
+    echo -e "\n\n=====================$d====================="
     if [[ -e $d/CMakeLists.txt ]]; then
         echo "this is C++ package"
         rm -rf $d
@@ -24,30 +24,26 @@ for d in $ws/src/*/ ; do
         fi
         echo $pkg
         if [[ $(grep "'" <<< $pkg) ]]; then
-            echo "single quote"
             pkg=$(cut -d "'" -f 2 <<< $pkg)
         else
-            echo "double quote"
             pkg=$(cut -d '"' -f 2 <<< $pkg)
         fi
-        echo $pkg
+        echo -e "\nsource files in <$pkg>:"
         rm -r $ws/build/$pkg
         rm -r $ws/install/$pkg
         mkdir -p $ws/install/$pkg/lib/python$python_version/site-packages
         mkdir $ws/backup/$pkg
-        rm -r $pkg.tmp
+        # rm -r $pkg.tmp
         mkdir $pkg.tmp
         touch $pkg.tmp/__init__.py
-# ==============================================================================================================================
-        # get package name from setup.py since it might be different from (basename $d)
         cd $pkg     # this is relative path, ws/src/pkg/pkg
-        rm compileList.txt
+        # rm compileList.txt
         for f in *.py ; do
             filename=$(basename $f)
             if [[ $filename == __init__.py ]]; then
                 continue
             fi
-            echo $f
+            echo "    $f"
             echo "#cython: language_level=3" > tmp.py
             echo "import aes_decrypt" >> tmp.py
             echo "aes_decrypt.decrypt()" >> tmp.py
@@ -65,14 +61,14 @@ for d in $ws/src/*/ ; do
             echo "if __name__ == '__main__':" >> "$filename.py"
             echo "    main()" >> "$filename.py"
         done
+        echo -e "\n+++++++++++++++ compile $pkg package +++++++++++++++"
         # still in source code directory of package, ws/src/pkg/pkg
         cp $ws/compile.py compile.py
         python3 compile.py build_ext --inplace
-        # move .so and use regex to get filename, so can move wrapper as well to tmp folder
+        echo -e "\n+++++++++++++++ compile finished, moving .so to site-packages +++++++++++++++"
         for so in *.so ; do
-            # mv $so $d/$pkg.tmp
             mv $so $ws/install/$pkg/lib/python$python_version/site-packages
-            echo "================================$so"
+            echo "$so"
             echo $(cut -d "." -f 1 <<< $so | cut -b 5-)
             mv $(cut -d "." -f 1 <<< $so | cut -b 5-).py $d/$pkg.tmp
         done
@@ -80,18 +76,14 @@ for d in $ws/src/*/ ; do
         cp $ws/$(ls $ws | grep .so) $ws/install/$pkg/lib/python$python_version/site-packages
 
         cd ..
-        # remove everything else
+        # remove temporary files
         rm -r $pkg
         mv $pkg.tmp $pkg
+        echo -e "\nfinished package $pkg\n\n"
     else
         echo "this is not a ROS2 package"
     fi
 done
 
-# build
 cd $ws
 colcon build
-
-# copy all *.so to right location (need to use python version)
-
-echo -e "\n\n---------------------\n"
